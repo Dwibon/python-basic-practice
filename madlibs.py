7b098e7e55f4a6075eaeee06dd7708fd21712e47
Dwibon <dwibonbhargabdeka@astu.ac.in>
2026-01-30 21:30:05 +0530

    Print the story

--- madlibs.py
+++ madlibs.py
@@ -6,9 +6,13 @@ place = input("Enter a place ")
 adjective_place = input("Enter an adjective for the place: ")
 plural_noun = input("Enter a plural noun: ")
 
-story = f"One ordinary day, I discovered I had a {adjective} superpower called {superpower}.\
-At first, I tested it on a small {noun}, and it instantly {verb}.\
+story = f"""
+One ordinary day, I discovered I had a {adjective} superpower called {superpower}.\
+At first, I tested it on a small {noun}, and it instantly {verb_present}.\
 Shocked but excited, I decided to use my {superpower} in public.\
 Soon, the entire {place} became {adjective_place}, and people started to notice.\
 Groups of {plural_noun} followed me everywhere, asking for help.\
-With great power came great responsibility, but being {adjective} made it all worthwhile."
+With great power came great responsibility, but being {adjective} made it all worthwhile.
+"""
+
+print(story)
